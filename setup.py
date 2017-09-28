c6d1d0e7150dfed4aee85df3efcc27ea0a33ead1
JeongUkJae <JeongUkJae@gmail.com>
2017-09-29 03:12:32 +0900

    fix 'build fail'

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-JWT-Login',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/JeongUkJae/Flask-JWT-Login',
     license='MIT',
     author='Jeong Ukjae',
     author_email='jeongukjae@gmail.com',
     description='Flask extension that helps authentication using JWT',
     packages=find_packages(exclude=['tests']),
-    long_description=open('README.md').read(),
     zip_safe=False,
     test_suite='nose.collector',
     include_package_data=True,
