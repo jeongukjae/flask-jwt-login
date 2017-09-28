7f61eb87deeffd3dad0a70ca463f6320042ddfb3
JeongUkJae <JeongUkJae@gmail.com>
2017-09-29 02:13:24 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='Flask-JWT-Login',
+    version='0.0.1',
+    url='https://github.com/JeongUkJae/Flask-JWT-Login',
+    license='MIT',
+    author='Jeong Ukjae',
+    author_email='jeongukjae@gmail.com',
+    description='Flask extension that helps authentification using JWT',
+    packages=find_packages(exclude=['tests']),
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    test_suite='nose.collector',
+    include_package_data=True,
+    install_requires=requirements,
+    tests_require=['nose'],
+)
