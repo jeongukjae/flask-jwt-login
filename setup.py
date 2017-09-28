a4e956b298d9f3a67698e25cf88831f4c42b189f
JeongUkJae <JeongUkJae@gmail.com>
2017-09-29 03:06:14 +0900

    edit version name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-JWT-Login',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/JeongUkJae/Flask-JWT-Login',
     license='MIT',
     author='Jeong Ukjae',
