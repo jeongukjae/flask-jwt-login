138dc87d6fde4135b0229cd0db697602538c5e59
JeongUkJae <JeongUkJae@gmail.com>
2017-10-21 22:18:42 +0900

    edit version

--- flask_jwt_login/__init__.py
+++ flask_jwt_login/__init__.py
@@ -3,4 +3,4 @@ from .jwt_login import JWT, login_required
 from .processing import process_login, get_current_user
 
 # module version
-__version__ = '0.0.3'
+__version__ = '0.0.4'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-JWT-Login',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/JeongUkJae/Flask-JWT-Login',
     license='MIT',
     author='Jeong Ukjae',
